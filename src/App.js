376a73cb69c7a0e462f78d77cb4a1b2aff8a2373
Letícia Damasio <damasioleticia03@gmail.com>
2022-03-03 10:55:48 -0300

    React Counter App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Contador extends Component {
+
+ state = {
+  count: 0,
+    mensagem: ""
+  }
+
+ add = () => {
+  const {count} = this.state
+    if (count < 10) {
+      this.setState ({
+       count: count +1
+       })
+     }
+  }
+ remove = () => {
+  const {count} = this.state
+   if (count > 0) {
+     this.setState ({
+       count: count -1
+     })
+   }
 }
-
-export default App;
+  render (){
+    const {count} = this.state
+    const {add, remove} = this
+    
+    return (
+      <div class="container">
+       <h1>React Counter App</h1>
+      <div class="counter">
+        <button class="button" onClick={add}>+</button>
+        <h2>{count}</h2>
+        <button class="button" onClick={remove}>-</button>
+      </div>
+      </div>
+    )
+  }
+}
